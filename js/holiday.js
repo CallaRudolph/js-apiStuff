39b247175351dc1b74c64931520f06c1b74d1d5d
Calla Rudolph and Nathan Stewart <nathan@>
2017-08-03 11:14:28 -0700

    resolve error message for fail API request

--- js/holiday.js
+++ js/holiday.js
@@ -7,7 +7,8 @@ Holiday.prototype.getHolidays = function(country, year, month, displayHolidays) 
   $.get('https://holidayapi.com/v1/holidays?key=' + apiKey + '&country=' + country + '&year=' + year + '&month=' + month).then(function(response) {
     displayHolidays(response.holidays);
   }).fail(function(error) {
-    $('.showResult').text(error.responseJSON.message);
+    $('.showResult').text(error.responseText);
+    console.log(error);
   });
 };
 
